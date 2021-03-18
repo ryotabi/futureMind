db2995ce54358d42eb84abd5d8875d8425614720
ryota <sryotapersian@gmail.com>
2021-03-18 18:51:59 +0900

    20210318/会社モデル追加

---
+++ app/models/Company.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Company extends Authenticatable
+{
+    use Notifiable;
+
+    public function diagnosis(){
+        return $this->hasOne('App\models\CompanyDiagnosisData','user_id');
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+     protected $guard = 'company';
+    protected $fillable = [
+        'name', 'email', 'password','company_icon','industry','office','employee','homepage','comment'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function likesStudent(){
+        return $this->belongsToMany('App\models\User','likes','company_id','user_id');
+    }
+    }
+
